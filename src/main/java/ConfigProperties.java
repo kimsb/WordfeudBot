ecbefd55ccdaac098c67beb8acdad7e76cf861a0
kimsb <kim.stephen.bovim@bekk.no>
2017-09-14 23:35:42 +0200

    dratt ut mdag til egen kode også...

--- src/main/java/ConfigProperties.java
+++ src/main/java/ConfigProperties.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigProperties {
@@ -6,14 +5,15 @@ public class ConfigProperties {
     private Properties properties = new Properties();
 
     public ConfigProperties() {
-        try {
-            properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public String getProperty(final String propertyName) {
-        return properties.getProperty(propertyName);
+//        return properties.getProperty(propertyName);
+        return System.getProperty(propertyName);
     }
 }
